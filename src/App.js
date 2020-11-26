6fae464c00268ef052de775f4d8f54df3d54415e
andredavid1 <andredavid1@yahoo.com.br>
2020-11-26 19:34:19 -0300

    rotas iniciais

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import MainPage from './pages/main'
+import Login from './pages/login'
 
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={MainPage} exact />
+        <Route path='/login' component={Login} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
