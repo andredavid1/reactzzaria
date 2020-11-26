a529854524eff99a6b280ff4272a818c2ed96fb7
andredavid1 <andredavid1@yahoo.com.br>
2020-11-26 20:10:25 -0300

    Dependências UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CssBaseline } from '@material-ui/core'
 
 import MainPage from 'pages/main'
 import Login from 'pages/login'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/login' component={Login} />
-        <Route component={MainPage} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route component={MainPage} />
+        </Switch>
+      </BrowserRouter>
+    </>
   )
 }
 
