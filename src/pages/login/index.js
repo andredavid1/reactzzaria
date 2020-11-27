c14f65765b98af2d6ceadd93ce56ceb2cae8e079
andredavid1 <andredavid1@yahoo.com.br>
2020-11-26 21:22:00 -0300

    initial design login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
+import { Grid, Button } from '@material-ui/core'
+import { ReactComponent as Logo } from './logo-react-zzaria.svg'
+
 const Login = () => (
-  <h1>Login</h1>
+  <>
+    <Grid container>
+      <Grid item>
+        <Logo />
+      </Grid>
+
+      <Grid item>
+        <Button>Entrar com GitHub</Button>
+      </Grid>
+    </Grid>
+  </>
 )
 
 export default Login
